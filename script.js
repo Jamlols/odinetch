b500d51fa1b2b8ce173e50d08a43aa25440b8a07
Kirk <76870582+Jamlols@users.noreply.github.com>
2023-10-29 17:09:40 +0000

    Add code to enable drawing function

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
 const square = document.querySelector("#container");
 
-for (i = 0; i < 16; i++) {
+for (i = 0; i < 256; i++) {
     drawingSquares = document.createElement("div");
-    drawingSquares.textContent = "hi";
     drawingSquares.classList.add("draw");
     square.appendChild(drawingSquares);
 }
 
+const highlight = document.getElementsByClassName("draw");
 
+let mouseIsDown = false;
+
+function handleMouseEnter(event) {
+    if (mouseIsDown) {
+        event.target.style.backgroundColor = "red";
+    }
+}
+
+function handleMouseDown(event) {
+    mouseIsDown = true;
+    event.target.style.backgroundColor = "red";
+}
+
+function handleMouseUp() {
+    mouseIsDown = false;
+}
+
+document.addEventListener("mousedown", handleMouseDown);
+document.addEventListener("mouseup", handleMouseUp);
+
+for (let element of highlight) {
+    element.addEventListener("mouseenter", handleMouseEnter);
+
+}
