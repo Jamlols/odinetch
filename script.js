bf3e4e9ec0dedb5b0431bbeaa150605dd5e7d8cc
Kirk <76870582+Jamlols@users.noreply.github.com>
2023-10-29 20:19:29 +0000

    Add many functions

--- script.js
+++ script.js
@@ -11,14 +11,18 @@ const highlight = document.getElementsByClassName("draw");
 let mouseIsDown = false;
 
 function handleMouseEnter(event) {
-    if (mouseIsDown) {
+    if (mouseIsDown && event.target.classList.contains('draw')) {
         event.target.style.backgroundColor = "red";
     }
 }
 
 function handleMouseDown(event) {
-    mouseIsDown = true;
-    event.target.style.backgroundColor = "red";
+    if (event.target.classList.contains('draw')) {
+        mouseIsDown = true;
+        event.target.style.backgroundColor = "red";
+        event.preventDefault();
+    }
+
 }
 
 function handleMouseUp() {
@@ -32,3 +36,48 @@ for (let element of highlight) {
     element.addEventListener("mouseenter", handleMouseEnter);
 
 }
+
+let clearButton = document.querySelector("#clear");
+
+function clear() {
+    let draws = document.getElementsByClassName("draw");
+
+    for (i = 0; i < draws.length; i++) {
+        draws[i].style.backgroundColor = "white";
+    }
+}
+
+clearButton.addEventListener('click', clear);
+
+gridSlider = document.querySelector("#gridRange");
+
+function adjustGridSize(newSize) {
+    square.innerHTML = '';
+
+    for (let i = 0; i < newSize * newSize; i++) {
+        const newSquare = document.createElement("div");
+        newSquare.classList.add("draw");
+        newSquare.addEventListener("mouseenter", handleMouseEnter);
+        square.appendChild(newSquare);
+    }
+
+    square.style.gridTemplateColumns = `repeat(${newSize}, 1fr)`;
+    square.style.gridTemplateRows = `repeat(${newSize}, 1fr)`;
+
+    document.addEventListener("mousedown", handleMouseDown);
+}
+
+adjustGridSize(gridSlider.value);
+
+gridSlider.addEventListener('input', function() {
+    let sliderValue = parseInt(gridSlider.value);
+    adjustGridSize(sliderValue);
+})
+
+const gridSizeDisplay = document.querySelector("#gridSizeDisplay");
+
+gridSlider.addEventListener('input', function() {
+    let newSize = parseInt(gridSlider.value);
+    gridSizeDisplay.textContent = newSize;
+    setupGrid(newSize);
+})
